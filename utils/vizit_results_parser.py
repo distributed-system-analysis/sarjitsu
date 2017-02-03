df7c5d7b7ac9951800a4831c3d4a6e395e05d126
Archit Sharma <arcsharm@redhat.com>
2017-02-03 11:45:48 +0530

    add basic Unicode error handling in vizit

--- utils/vizit_results_parser.py
+++ utils/vizit_results_parser.py
@@ -7,8 +7,7 @@ from prettytable import PrettyTable
 
 fmt="%X, %d %b %Y"
 PT = PrettyTable()
-PT.field_names = ["Status", "Filename", "Platform",
-"Nodename", "Begin", "End"]
+PT.field_names = ["Status", "Filename", "Platform", "Nodename", "Begin", "End"]
 
 def tstos(date):
     date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
@@ -26,12 +25,32 @@ def display_results(data):
 
 
 if __name__ == '__main__':
-    result = json.loads(input())
-    display_results(result['data'])
-    print(PT)
-    if result['valid_results']:
-        # print("\nGrafana Dashboard at: %s" %
-        #       result['redirect_url'])
-        print("Login with your Grafana credentials [Default: (admin/admin)] to check the results.\n")
-    else:
-        print("\nValid results NOT found; No Dashboards generated.")
+    try:
+        curl_output = input()
+        result = json.loads(curl_output)
+        display_results(result['data'])
+        print(PT)
+        if result['valid_results']:
+            print("\nGrafana Dashboard at: %s" %
+                  result['redirect_url'])
+            print("Login with your Grafana credentials [Default: (admin/admin)].\n")
+        else:
+            print("\nValid results NOT found; No Dashboards generated.")
+        server_response_file = 'results.txt'
+        with open(server_response_file, 'w') as f:
+            f.write(str(PT)+"\n")
+        print("..dumped results into %s" % server_response_file)
+
+    except UnicodeDecodeError as e:
+        print("UnicodeDecodeError: unable to parse output of curl for some reason.")
+        print("\tERROR: %s" % e)
+        server_response_file = 'response_log.txt'
+        error_logfile = 'error.log'
+        with open(server_response_file, 'w') as f:
+            f.write(str(curl_output))
+        with open(error_logfile, 'w') as f:
+            f.write(str(e))
+        print("..dumped server response into %s" % server_response_file)
+        print("..dumped error log into %s" % error_logfile)
+    except:
+        raise
