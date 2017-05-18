23580db2a1d1e176d2cfb125470f1a78268a16f2
Archit Sharma <arcsharm@redhat.com>
2017-05-18 19:09:55 +0530

    init non-root container conversion

--- lib/datasource/docker-entrypoint.sh
+++ lib/datasource/docker-entrypoint.sh
@@ -4,45 +4,48 @@ set -e
 
 # Add elasticsearch as command if needed
 if [ "${1:0:1}" = '-' ]; then
-	set -- elasticsearch "$@"
+    set -- elasticsearch "$@"
 fi
 
 sed -i "s/# network.host:.*/network.host: 0.0.0.0/g" \
-        /etc/elasticsearch/elasticsearch.yml
+    /etc/elasticsearch/elasticsearch.yml
 
 # sed -i -r 's#elasticsearch\$RANDOM#'$ES_CLUSTER_NAME'#g' /etc/elasticsearch/elasticsearch.yml
 sed -i -r "s/#cluster.name: elasticsearch/cluster.name: elasticsearch$RANDOM/g" \
-          /etc/elasticsearch/elasticsearch.yml
+    /etc/elasticsearch/elasticsearch.yml
 
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
 
 # if [ "$1" = 'elasticsearch' ]; then
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-	# Change the ownership of user-mutable directories to elasticsearch
-	# for path in \
+    # Change the ownership of user-mutable directories to elasticsearch
+    # for path in \
 	# 	/usr/share/elasticsearch/data \
 	# 	/usr/share/elasticsearch/logs \
 	# ; do
-	# 	chown -R elasticsearch:elasticsearch "$path"
-	# done
-  echo "through PATH"
-  ES_HOME=/usr/share/elasticsearch
-  CONF_DIR=/etc/elasticsearch
-  CONF_FILE=/etc/elasticsearch/elasticsearch.yml
-  DATA_DIR=/var/lib/elasticsearch
-  LOG_DIR=/var/log/elasticsearch
-  PID_DIR=/var/run/elasticsearch
-	set -- elasticsearch \
-                -Des.pidfile=$PID_DIR/elasticsearch.pid \
-                -Des.default.path.home=$ES_HOME \
-                -Des.default.path.logs=$LOG_DIR \
-                -Des.default.path.data=$DATA_DIR \
-                -Des.default.config=$CONF_FILE \
-                -Des.default.path.conf=$CONF_DIR
-  # exec su-exec elasticsearch "$BASH_SOURCE" "$@"
+    # 	chown -R elasticsearch:elasticsearch "$path"
+    # done
+    echo "through PATH"
+    ES_HOME=/usr/share/elasticsearch
+    CONF_DIR=/etc/elasticsearch
+    CONF_FILE=/etc/elasticsearch/elasticsearch.yml
+    DATA_DIR=/var/lib/elasticsearch
+    LOG_DIR=/var/log/elasticsearch
+    PID_DIR=/var/run/elasticsearch
+
+    chown -R elasticsearch:elasticsearch /var/{run,lib,log}/elasticsearch /etc/elasticsearch
+    
+    set -- elasticsearch \
+        -Des.pidfile=$PID_DIR/elasticsearch.pid \
+        -Des.default.path.home=$ES_HOME \
+        -Des.default.path.logs=$LOG_DIR \
+        -Des.default.path.data=$DATA_DIR \
+        -Des.default.config=$CONF_FILE \
+        -Des.default.path.conf=$CONF_DIR
+    # exec su-exec elasticsearch "$BASH_SOURCE" "$@"
 else
-  echo "through SYSTEMD / init"
+    echo "through SYSTEMD / init"
 fi
 
 # As argument is not related to elasticsearch,
--- lib/middleware/docker-entrypoint.sh
+++ lib/middleware/docker-entrypoint.sh
@@ -4,52 +4,62 @@ set -e
 
 
 log(){
-  echo -e "[$(date +'%D %H:%M:%S %Z')] - $*"
+    echo -e "[$(date +'%D %H:%M:%S %Z')] - $*"
 }
 
 # Add as command if needed
 if [ "${1:0:1}" = '-' ]; then
-	set -- api_engine "$@"
+    set -- api_engine "$@"
 fi
 
 update_configs(){
-  if [[ ! -z $ES_HOST ]]; then
-    sed -i -r 's#^host\s?=.*#host = '$ES_HOST'#g' /etc/sar-index.cfg
-  else
-    sed -i -r 's#^host\s?=.*#host = datasource#g' /etc/sar-index.cfg
-  fi
+    if [[ ! -z $ES_HOST ]]; then
+	sed -i -r 's#^host\s?=.*#host = '$ES_HOST'#g' /etc/sar-index.cfg
+    else
+	sed -i -r 's#^host\s?=.*#host = datasource#g' /etc/sar-index.cfg
+    fi
 
-  sed -i -r 's#^port\s?=.*#port = '$ES_PORT'#g' /etc/sar-index.cfg
-  sed -i -r 's#^protocol\s?=.*#protocol = '$ES_PROTOCOL'#g' /etc/sar-index.cfg
+    sed -i -r 's#^port\s?=.*#port = '$ES_PORT'#g' /etc/sar-index.cfg
+    sed -i -r 's#^protocol\s?=.*#protocol = '$ES_PROTOCOL'#g' /etc/sar-index.cfg
 
-  sed -i -r 's#^index_prefix\s?=.*#index_prefix = '$INDEX_PREFIX'#g' /etc/sar-index.cfg
-  sed -i -r 's#^index_version\s?=.*#index_version = '$BULK_ACTION_COUNT'#g' /etc/sar-index.cfg
-  sed -i -r 's#^bulk_action_count\s?=.*#bulk_action_count = '$INDEX_VERSION'#g' /etc/sar-index.cfg
-  sed -i -r 's#^number_of_shards\s?=.*#number_of_shards = '$SHARD_COUNT'#g' /etc/sar-index.cfg
-  sed -i -r 's#^number_of_replicas\s?=.*#number_of_replicas = '$REPLICAS_COUNT'#g' /etc/sar-index.cfg
+    sed -i -r 's#^index_prefix\s?=.*#index_prefix = '$INDEX_PREFIX'#g' /etc/sar-index.cfg
+    sed -i -r 's#^index_version\s?=.*#index_version = '$BULK_ACTION_COUNT'#g' /etc/sar-index.cfg
+    sed -i -r 's#^bulk_action_count\s?=.*#bulk_action_count = '$INDEX_VERSION'#g' /etc/sar-index.cfg
+    sed -i -r 's#^number_of_shards\s?=.*#number_of_shards = '$SHARD_COUNT'#g' /etc/sar-index.cfg
+    sed -i -r 's#^number_of_replicas\s?=.*#number_of_replicas = '$REPLICAS_COUNT'#g' /etc/sar-index.cfg
 
-  sed -i -r 's#^ds_name\s?=.*#ds_name = '$GRAFANA_DS_NAME'#g' /etc/sar-index.cfg
-  sed -i -r 's#^pattern\s?=.*#pattern = '$GRAFANA_DS_PATTERN'#g' /etc/sar-index.cfg
-  sed -i -r 's#^timeField\s?=.*#timeField = '$GRAFANA_TIMEFIELD'#g' /etc/sar-index.cfg
+    sed -i -r 's#^ds_name\s?=.*#ds_name = '$GRAFANA_DS_NAME'#g' /etc/sar-index.cfg
+    sed -i -r 's#^pattern\s?=.*#pattern = '$GRAFANA_DS_PATTERN'#g' /etc/sar-index.cfg
+    sed -i -r 's#^timeField\s?=.*#timeField = '$GRAFANA_TIMEFIELD'#g' /etc/sar-index.cfg
 
-  if [[ ! -z $DB_HOST ]]; then
-    sed -i -r 's#^db_host\s?=.*#db_host = '$DB_HOST'#g' /etc/sar-index.cfg
-  else
-    sed -i -r 's#^db_host\s?=.*#db_host = metricstore#g' /etc/sar-index.cfg
-  fi
-  sed -i -r 's#^db_name\s?=.*#db_name = '$DB_NAME'#g' /etc/sar-index.cfg
-  sed -i -r 's#^db_user\s?=.*#db_user = '$DB_USER'#g' /etc/sar-index.cfg
-  sed -i -r 's#^db_password\s?=.*#db_password = '$DB_PASSWORD'#g' /etc/sar-index.cfg
-  sed -i -r 's#^db_port\s?=.*#db_port = '$DB_PORT'#g' /etc/sar-index.cfg
+    if [[ ! -z $DB_HOST ]]; then
+	sed -i -r 's#^db_host\s?=.*#db_host = '$DB_HOST'#g' /etc/sar-index.cfg
+    else
+	sed -i -r 's#^db_host\s?=.*#db_host = metricstore#g' /etc/sar-index.cfg
+    fi
+    sed -i -r 's#^db_name\s?=.*#db_name = '$DB_NAME'#g' /etc/sar-index.cfg
+    sed -i -r 's#^db_user\s?=.*#db_user = '$DB_USER'#g' /etc/sar-index.cfg
+    sed -i -r 's#^db_password\s?=.*#db_password = '$DB_PASSWORD'#g' /etc/sar-index.cfg
+    sed -i -r 's#^db_port\s?=.*#db_port = '$DB_PORT'#g' /etc/sar-index.cfg
 
-  sed -i -r 's#^api_port\s?=.*#api_port = '$MIDDLEWARE_PORT'#g' /etc/sar-index.cfg
+    sed -i -r 's#^api_port\s?=.*#api_port = '$MIDDLEWARE_PORT'#g' /etc/sar-index.cfg
 }
 
-if [ "$1" = 'api_engine' -a "$(id -u)" = '0' ]; then
-  update_configs
-  /scripts/es-metadata-handler
-  cd /opt/api_server
-  /opt/api_server/run.py
+# -a "$(id -u)" = '0'
+
+if [ "$1" = 'api_engine' ]; then
+    
+    export USER_ID=$(id -u)
+    export GROUP_ID=$(id -g)
+    envsubst < /passwd.template > /tmp/passwd
+    export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
+    export NSS_WRAPPER_PASSWD=/tmp/passwd
+    export NSS_WRAPPER_GROUP=/etc/group
+
+    update_configs
+    /scripts/es-metadata-handler
+    cd /opt/api_server
+    /opt/api_server/run.py
 fi
 
 exec "$@"
